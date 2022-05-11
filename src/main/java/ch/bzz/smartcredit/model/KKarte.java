07912cb5fca036c86a52ee3d4c65d4306bf79ade
francescoryu <ryuf@bzz.ch>
2022-05-11 11:31:01 +0200

    Final Commit

--- src/main/java/ch/bzz/smartcredit/model/KKarte.java
+++ src/main/java/ch/bzz/smartcredit/model/KKarte.java
@@ -4,4 +4,28 @@ public class KKarte {
     private String KKarteUUID;
     private String institut;
     private String kartenNummer;
+
+    public String getKKarteUUID() {
+        return KKarteUUID;
+    }
+
+    public String getInstitut() {
+        return institut;
+    }
+
+    public String getKartenNummer() {
+        return kartenNummer;
+    }
+
+    public void setKKarteUUID(String KKarteUUID) {
+        this.KKarteUUID = KKarteUUID;
+    }
+
+    public void setInstitut(String institut) {
+        this.institut = institut;
+    }
+
+    public void setKartenNummer(String kartenNummer) {
+        this.kartenNummer = kartenNummer;
+    }
 }
--- src/main/java/ch/bzz/smartcredit/model/Kunde.java
+++ src/main/java/ch/bzz/smartcredit/model/Kunde.java
@@ -8,4 +8,44 @@ public class Kunde {
     private String vorName;
     private String nachName;
     private Integer alter;
+
+    public String getKundeUUID() {
+        return kundeUUID;
+    }
+
+    public List<KKarte> getKartenListe() {
+        return kartenListe;
+    }
+
+    public String getVorName() {
+        return vorName;
+    }
+
+    public String getNachName() {
+        return nachName;
+    }
+
+    public Integer getAlter() {
+        return alter;
+    }
+
+    public void setKundeUUID(String kundeUUID) {
+        this.kundeUUID = kundeUUID;
+    }
+
+    public void setKartenListe(List<KKarte> kartenListe) {
+        this.kartenListe = kartenListe;
+    }
+
+    public void setVorName(String vorName) {
+        this.vorName = vorName;
+    }
+
+    public void setNachName(String nachName) {
+        this.nachName = nachName;
+    }
+
+    public void setAlter(Integer alter) {
+        this.alter = alter;
+    }
 }
